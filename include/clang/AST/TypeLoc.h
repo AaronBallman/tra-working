870764c53d5634dafe3251068e150a11f31a115f
Reid Kleckner <reid@kleckner.net>
2013-06-24 17:51:48 +0000

    [AST] Introduce a new DecayedType sugar node
    The goal of this sugar node is to be able to look at an arbitrary
    FunctionType and tell if any of the parameters were decayed from an
    array or function type.  Ultimately this is necessary to implement
    Microsoft's C++ name mangling scheme, which mangles decayed arrays
    differently from normal pointers.
    
    Reviewers: rsmith
    
    Differential Revision: http://llvm-reviews.chandlerc.com/D1014
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@184763 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/clang/AST/TypeLoc.h
+++ include/clang/AST/TypeLoc.h
@@ -978,7 +978,7 @@ inline TypeLoc TypeLoc::IgnoreParens() const {
 struct DecayedLocInfo { }; // Nothing.
 
 /// \brief Wrapper for source info for pointers decayed from arrays and
-/// functions.
+/// funcitons.
 class DecayedTypeLoc : public ConcreteTypeLoc<UnqualTypeLoc, DecayedTypeLoc,
                                               DecayedType, DecayedLocInfo> {
 public:
